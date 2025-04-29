d798b8f89f33504793aac91f2fc2a0eaeb629f73
Brett Guenther <bguenther@google.com>
2025-04-29 11:30:03 +1000

    js react upgrade

--- templates/react-javascript/src/App.js
+++ templates/react-javascript/src/App.js
@@ -20,13 +20,13 @@
 
 import React from 'react'
 import { HelloWorld } from './HelloWorld'
-import { ExtensionProvider } from '@looker/extension-sdk-react'
+import { ExtensionProvider40 } from '@looker/extension-sdk-react'
 import { hot } from 'react-hot-loader/root'
 
 export const App = hot(() => {
   return (
-    <ExtensionProvider>
+    <ExtensionProvider40>
       <HelloWorld />
-    </ExtensionProvider>
+    </ExtensionProvider40>
   )
 })
--- templates/react-javascript/src/HelloWorld.js
+++ templates/react-javascript/src/HelloWorld.js
@@ -14,20 +14,20 @@
 
 import React, { useEffect, useState, useContext } from 'react'
 import { Space, ComponentsProvider, Span } from '@looker/components'
-import { ExtensionContext } from '@looker/extension-sdk-react'
+import { getCore40SDK } from '@looker/extension-sdk-react'
 
 /**
  * A simple component that uses the Looker SDK through the extension sdk to display a customized hello message.
  */
 
 export const HelloWorld = () => {
-  const { core40SDK } = useContext(ExtensionContext)
+  const coreSDK = getCore40SDK()
   const [message, setMessage] = useState()
 
   useEffect(() => {
     const initialize = async () => {
       try {
-        const value = await core40SDK.ok(core40SDK.me())
+        const value = await coreSDK.ok(coreSDK.me())
         setMessage(`Hello, ${value.display_name}`)
       } catch (error) {
         setMessage('Error occured getting information about me!')
